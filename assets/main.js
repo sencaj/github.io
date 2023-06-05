fe5fe0e75ceb26b59ff2988be9465e42930b39bc
VS <vlado.sencaj@gmail.com>
2023-06-05 03:41:02 +0200

    Add files via upload

--- assets/main.js
+++ assets/main.js
@@ -25,6 +25,10 @@ google = document.createElement('script')
 google.innerText = "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-490CDYV0P3');"
 head.appendChild(google)
 
+var upwork = document.createElement('script')
+upwork.setAttribute('type','text/javascript')
+upwork.innerText = "(function (i, m, p, a, c, t) { c.ire_o = p; c[p] = c[p] || function () { (c[p].a = c[p].a || []).push(arguments) }; t = a.createElement(m); var z = a.getElementsByTagName(m)[0]; t.async = 1; t.src = i; z.parentNode.insertBefore(t, z) })('https://utt.impactcdn.com/P-A4345834-f732-48db-8ea6-7128d22cd3541.js', 'script', 'impactStat', document, window); impactStat('transformLinks'); impactStat('trackImpression');"
+head.appendChild(upwork)
 
 // Create cookie
 function setCookie(cname, cvalue, exdays) {
